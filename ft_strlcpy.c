e41ab9faf271587f50e67f324ca3a61d7b614b47
Hervé <herve.risse91@gmail.com>
2024-11-27 17:43:16 +0100

    tester update

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -15,21 +15,20 @@
 size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t i;
+	size_t src_len;
 
-	i = 0;
+	src_len = ft_strlen(src);
 	if (size == 0)
-		return (ft_strlen(src));
+		return (src_len);
+	i = 0;
 	while (src[i] && i < size - 1)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	while (i < size)
-	{
+	if (size > 0)
 		dst[i] = '\0';
-		i++;
-	}
-	return (ft_strlen(src));
+	return (src_len);
 }
 
 /*
